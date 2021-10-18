e87834c2fbeece9d217f55771bf2c17851a4542e
rhit-huangmj <huangmj@rose-hulman.edu>
2021-10-18 10:49:54 -0400

    menus collapse, no css yet

---
+++ public/scripts/navigator.js
@@ -0,0 +1,26 @@
+//https://codemyui.com/directory-list-with-collapsible-nested-folders-and-files/
+var allFolders = $(".directory-list li > ul");
+allFolders.each(function () {
+
+    // add the folder class to the parent <li>
+    var folderAndName = $(this).parent();
+    folderAndName.addClass("folder");
+
+    // backup this inner <ul>
+    var backupOfThisFolder = $(this);
+    // then delete it
+    $(this).remove();
+    // add an <a> tag to whats left ie. the folder name
+    folderAndName.wrapInner("<a href='#' />");
+    // then put the inner <ul> back
+    folderAndName.append(backupOfThisFolder);
+    
+
+    // now add a slideToggle to the <a> we just added
+    folderAndName.find("a").click(function (e) {
+        $(this).siblings("ul").slideToggle("fast");
+        e.preventDefault();
+    });
+    folderAndName.find("a").trigger('click');
+
+});

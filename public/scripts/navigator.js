abeb6f3319499cc561c2ed195abd6607969186d7
rhit-huangmj <huangmj@rose-hulman.edu>
2021-10-20 13:33:44 -0400

    the nav button works
    the nav button(only viewable on small screens) will open/close the left navigator and hide the main body

--- public/scripts/navigator.js
+++ public/scripts/navigator.js
@@ -14,7 +14,7 @@ allFolders.each(function () {
     folderAndName.wrapInner("<a href='#' />");
     // then put the inner <ul> back
     folderAndName.append(backupOfThisFolder);
-    
+
 
     // now add a slideToggle to the <a> we just added
     folderAndName.find("a").click(function (e) {
@@ -24,3 +24,33 @@ allFolders.each(function () {
     folderAndName.find("a").trigger('click');
 
 });
+
+var listOpen = false;
+document.querySelector("#navButton").onclick = (event) => {
+    if (!listOpen) {
+        document.querySelector(".body").style.display = "none";
+        document.querySelector(".leftNavigator").style.display = "contents";
+        document.querySelector("body").style.backgroundColor = "#a7a7a7";
+    } else {
+        document.querySelector(".body").style.display = "contents";
+        document.querySelector(".leftNavigator").style.display = "none";
+        document.querySelector("body").style.backgroundColor = "#DDDDDD";
+    }
+    listOpen = !listOpen;
+};
+
+function myFunction(x) {
+    if (x.matches) { // If media query matches
+        document.querySelector(".body").style.display = "flex";
+        document.querySelector(".leftNavigator").style.display = "flex";
+        document.querySelector("body").style.backgroundColor = "#DDDDDD";
+    } else {
+        document.querySelector(".leftNavigator").style.display = "none";
+        document.querySelector(".body").style.display = "contents";
+        listOpen = false;
+    }
+}
+
+var x = window.matchMedia("(min-width: 450px)");
+myFunction(x); // Call listener function at run time
+x.addListener(myFunction); // Attach listener function on state changes

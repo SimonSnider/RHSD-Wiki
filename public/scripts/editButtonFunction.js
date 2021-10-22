df0619aa4657c002e11afc93670d467d5ce0a9a8
rhit-huangmj <huangmj@rose-hulman.edu>
2021-10-22 17:36:44 -0400

    editPagesList.html + functionality

--- public/scripts/editButtonFunction.js
+++ public/scripts/editButtonFunction.js
@@ -1,25 +1,18 @@
-rhit.editFoldersManager = class {
+rhit.editFoldersController = class {
     constructor() {
-        document.querySelector("#editButton").onclick = (event) => {
-            console.log("editButton Pushed");
-            window.location.href = "/editFoldersList.html";
-        };
-        rhit.fbFolderManager.beginListening(this.populateList.bind(this));
-        this.populateList();
+        rhit.fbFolderManager.beginListening(this.populateFoldersList.bind(this));
+        this.populateFoldersList();
     }
 
-    populateList() {
+    populateFoldersList() {
         const newList = htmlToElement('<ul class="folderList"></ul>')
         for (let i = 0; i < rhit.fbFolderManager.length; i++) {
             const folder = rhit.fbFolderManager.getFolderAtIndex(i);
             const newFolderButton = this._createFolderButton(folder);
-            // newFolderItem.querySelector("a").onclick = (e) => {
-            // 	console.log("folder click");
-            // 	newFolderItem.querySelector("ul").slideToggle("fast");
-            // 	e.preventDefault();
-            // }
             newFolderButton.onclick = (event) => {
-                newFolderButton.focus();
+                console.log(`${folder.id}`);
+                window.location.href = `/editPagesList.html?fid=${folder.id}`;
+
             }
             newList.appendChild(newFolderButton);
 
@@ -30,22 +23,82 @@ rhit.editFoldersManager = class {
         const oldList = document.querySelector(".folderList");
         oldList.parentElement.appendChild(newList);
         oldList.remove();
-    };
+    }
 
     _createFolderButton(folder) {
         return htmlToElement(`<a href="#" class="list-group-item folderListItem">${folder.name}</a>`);
-    };
+    }
 
     _createBar() {
         return htmlToElement(`<div class="input-group mb-3">
-                                <input type="text" class="form-control" placeholder="Folder name"
+                                <input type="text" id="folderName" class="form-control" placeholder="New Folder Name"
                                  aria-label="Recipient's username" aria-describedby="basic-addon2">
                                     <div class="input-group-append">
                                          <button class="btn btn-outline-secondary" type="button"><i class="material-icons">done</i></button>
                                     </div>
                             </div>`);
-    };
+    }
 
 }
 
-rhit.editFoldersManager = new rhit.editFoldersManager();
+rhit.editPagesController = class {
+    constructor(folderid) {
+        this.folderID = folderid;
+        rhit.fbPageManager.beginListening(this.populatePagesList.bind(this));
+        this.populatePagesList();
+    }
+
+    populatePagesList() {
+        const pages = rhit.fbPageManager.getPagesByFolder(this.folderID);
+        const newList = htmlToElement('<ul class="pageList"></ul>');
+        for (let i = 0; i < pages.length; i++) {
+            const page = pages[i];
+            const newPageButton = this._createPageButton(page);
+            // newFolderButton.onclick = (event) => {
+            //     newFolderButton.focus();
+            //     // document.querySelector("#folderName").value = folder.name;
+            //     console.log(`${folder.id}`);
+            //     window.location.href = `/editPagesList.html?fid=${folder.id}`;
+
+            // }
+            newList.appendChild(newPageButton);
+
+        }
+        const editBar = this._createBar();
+        newList.appendChild(editBar);
+
+        const oldList = document.querySelector(".pageList");
+        oldList.parentElement.appendChild(newList);
+        oldList.remove();
+    }
+
+    _createPageButton(page) {
+        return htmlToElement(`<a href="#" class="list-group-item folderListItem">${page.name}</a>`);
+    }
+
+    _createBar() {
+        return htmlToElement(`<div class="input-group mb-3">
+                                <input type="text" id="folderName" class="form-control" placeholder="New Page Name"
+                                 aria-label="Recipient's username" aria-describedby="basic-addon2">
+                                    <div class="input-group-append">
+                                         <button class="btn btn-outline-secondary" type="button"><i class="material-icons">done</i></button>
+                                    </div>
+                            </div>`);
+    }
+
+}
+
+if (document.querySelector("#editPagesListPage")) {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const folderID = urlParams.get("fid");
+    rhit.editPagesController = new rhit.editPagesController(folderID);
+}
+if (document.querySelector("#editFoldersListPage")) {
+    rhit.editFoldersController = new rhit.editFoldersController();
+}
+
+document.querySelector("#editButton").onclick = (event) => {
+    console.log("editButton Pushed");
+    window.location.href = "/editFoldersList.html";
+};
